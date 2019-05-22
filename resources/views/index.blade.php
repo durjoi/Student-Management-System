2af8efae41446fa5434ec780a51b4e92e107830a
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2019-05-22 17:14:27 +0600

    adding Bootstrap

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Student Management System</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+  </head>
+  <body>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-6 offset-3 text-center">
+          <h2>Hello Laravel</h2>
+        </div>
+      </div>
+    </div>
+
+
+    <script type="text/javascript" src=" {{ asset('js/bootstrap.min.js') }} "></script>
+  </body>
+</html>
